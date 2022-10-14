7c9fa53b75cbcfe7d14a2e5a803f962e705595a1
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-14 11:55:59 -0300

    RecyclerView configuration

--- app/src/main/java/com/myminiblog/HomeFragment.kt
+++ app/src/main/java/com/myminiblog/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -24,6 +25,7 @@ class HomeFragment : Fragment() {
     private lateinit var mBinding: FragmentHomeBinding
 
     private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,6 +47,8 @@ class HomeFragment : Fragment() {
             private lateinit var mContext: Context
 
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                mContext = parent.context
+
                 val view = LayoutInflater.from(mContext)
                     .inflate(R.layout.item_snapshot, parent, false)
                 return SnapshotHolder(view)
@@ -82,6 +86,26 @@ class HomeFragment : Fragment() {
                 //Snackbar.make(mBinding.root, error.message, Snackbar.LENGTH_SHORT).show()
             }
         }
+
+        //configuracion del recyclerView
+        mLayoutManager = LinearLayoutManager(context)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mLayoutManager
+            adapter = mFirebaseAdapter
+        }
+    }
+
+    //cuando se van a comenzar a consumir los datos
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAdapter.stopListening()
     }
 
     inner class SnapshotHolder(view: View) : RecyclerView.ViewHolder(view) {
