5f838238dd7e2aa657f88509f6518c82f65fe83a
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-14 15:34:54 -0300

    Fixed auth added

--- app/src/main/java/com/myminiblog/MainActivity.kt
+++ app/src/main/java/com/myminiblog/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.myminiblog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.myminiblog.databinding.ActivityMainBinding
 import java.util.Arrays
@@ -102,5 +105,21 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
+    //devolucion de auth
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Bienvenido...", Toast.LENGTH_SHORT).show()
+            } else {
+                //backPress
+                if (IdpResponse.fromResultIntent(data) == null) {
+                    finish()
+                }
+
+            }
+        }
     }
 }
